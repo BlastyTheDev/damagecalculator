449c0e8e64fd3d5be26fbe7b0b3071c61387b45b
BlastyTheDev <rmzha2234@gmail.com>
2024-07-17 23:30:38 +1000

    debugging, seems to be inaccurate right now

--- genshin.py
+++ genshin.py
@@ -1,24 +1,39 @@
+import numpy as np
+
+def debug():
+    print(
+        (1636 * 0.693) * (1 + 0 - 0.1) * (1 - ((5 * 75 + 500) / ((5 * 75 + 500) + 5 * 90 + 500)))
+    )
+
+
+
 def main():
     scaling_stat_value: float = float(input('Scaling stat value: '))
     ability_scaling_percent: float = float(input('Ability scaling percent: ')) / 100
-    base_dmg_multiplier: float = float(input('Base dmg multiplier: '))
-    additive_base_dmg_bonus: float = float(input('Additive base dmg bonus: '))
+
+    base_dmg = scaling_stat_value * ability_scaling_percent
+
+    # base_dmg_multiplier: float = float(input('Base dmg multiplier: '))
+    base_dmg_multiplier = 1
+    # additive_base_dmg_bonus: float = float(input('Additive base dmg bonus: '))
+    additive_base_dmg_bonus = 0
     dmg_bonus_percent: float = float(input('Dmg bonus percent: '))
 
     level_character: int = int(input('Character level: '))
     level_enemy: int = int(input('Enemy level: '))
 
-    res_base_enemy: float = float(input('Enemy base RES: '))
-    res_bonus_enemy: float = float(input('Enemy bonus RES: '))
-    res_debuffs_enemy: float = float(input('Enemy RES debuffs: '))
+    res_base_enemy: float = float(input('Enemy base RES: ')) / 100
+    res_bonus_enemy: float = float(input('Enemy bonus RES: ')) / 100
+    res_debuffs_enemy: float = float(input('Enemy RES debuffs: ')) / 100
 
-    defense_ignored: float = float(input('Defense ignored: '))
-    defense_dmg_reduction = get_defense_dmg_reduction(scaling_stat_value, level_enemy)
-    defense_multiplier_target = get_defense_multiplier(level_character, level_enemy, defense_dmg_reduction, defense_ignored)
+    # defense_ignored: float = float(input('Defense ignored: '))
+    defense_ignored = 0
+    defense_dmg_reduction = get_defense_dmg_reduction(get_enemy_defense(level_enemy), level_character)
+    defense_multiplier_target = get_defense_multiplier(level_character)
     res_multiplier_target = get_res_multiplier(get_res_percentage(res_base_enemy, res_bonus_enemy, res_debuffs_enemy))
     amplifying_multiplier = 1  # 1 if no elemental reactions
 
-    print(f'Raw DMG: {get_raw_dmg(ability_scaling_percent, scaling_stat_value, base_dmg_multiplier, additive_base_dmg_bonus,
+    print(f'Raw DMG: {get_raw_dmg(base_dmg, base_dmg_multiplier, additive_base_dmg_bonus,
                                   get_dmg_bonus_multiplier(dmg_bonus_percent, defense_dmg_reduction),
                                   defense_multiplier_target, res_multiplier_target, amplifying_multiplier)}')
 
@@ -35,10 +50,6 @@ def get_res_multiplier(res_percentage: float) -> float:
     return 1 / (4 * res_percentage + 1)
 
 
-def get_base_dmg(ability_scaling_percent: float, scaling_stat_value: float) -> float:
-    return ability_scaling_percent * scaling_stat_value
-
-
 def get_dmg_bonus_multiplier(dmg_bonus_percent: float, dmg_reduction_target: float) -> float:
     return 1 + dmg_bonus_percent - dmg_reduction_target
 
@@ -51,15 +62,18 @@ def get_enemy_defense(enemy_level: int) -> int:
     return 5 * enemy_level + 500
 
 
-def get_defense_multiplier(level_character: int, level_enemy: int, defense_reduction: float, defense_ignored: float) -> float:
-    return (level_character + 100) / ((1 - defense_reduction) * (1 - defense_ignored)) * (level_enemy + 100) + (level_character + 100)
+# def get_defense_multiplier(level_character: int, level_enemy: int, defense_reduction: float, defense_ignored: float) -> float:
+#     return (level_character + 100) / ((1 - defense_reduction) + (1 - defense_ignored)) * (level_enemy + 100) + (level_character + 100)
 
 
-def get_raw_dmg(ability_scaling_percent: float, scaling_stat_value: float, base_dmg_multiplier: float,
+def get_defense_multiplier(defense_dmg_reduction: float) -> float:
+    return 1 - defense_dmg_reduction
+
+
+def get_raw_dmg(base_dmg: float, base_dmg_multiplier: float,
                 additive_base_dmg_bonus: float, dmg_bonus_multiplier: float, def_multiplier_target: float,
                 res_multiplier_target: float, amplifying_multiplier: float) -> float:
-    return ((sum(get_base_dmg(ability_scaling_percent, scaling_stat_value),
-                 base_dmg_multiplier) + additive_base_dmg_bonus)
+    return ((np.sum(base_dmg * base_dmg_multiplier) + additive_base_dmg_bonus)
             * dmg_bonus_multiplier * def_multiplier_target * res_multiplier_target * amplifying_multiplier)
 
 
@@ -68,4 +82,5 @@ def get_crit_hit(dmg: float, crit_dmg_percent: float) -> float:
 
 
 if __name__ == "__main__":
-    main()
+    debug()
+    # main()
